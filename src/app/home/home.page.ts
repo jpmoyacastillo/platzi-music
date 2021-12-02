4038e210cb2ee95692e28d8499e0683fd2231ec4
jpmoya <juanpablo@rindegastos.com>
2021-12-02 20:20:34 +0100

    Swipe slides OK

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,6 +12,34 @@ export class HomePage {
     centeredSlides: true,
     speed: 400,
   };
+
+  slides = [
+    {
+    imageSrc: '../../assets/img/logo.png',
+    imageAlt: 'Platzi Music Logo',
+    title: 'Escucha tu música',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
+    icon: 'play',
+    },
+    {
+    imageSrc: '../../assets/img/logo.png',
+    imageAlt: 'Platzi Music Logo',
+    title: 'Escucha tu música',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
+    icon: 'play',
+    },
+    {
+    imageSrc: '../../assets/img/logo.png',
+    imageAlt: 'Platzi Music Logo',
+    title: 'Escucha tu música',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
+    icon: 'play',
+    },
+
+  ];
   constructor() {}
 
 }
