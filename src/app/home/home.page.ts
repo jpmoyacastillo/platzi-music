988b0396b4c43a1cfaa2ce5607ec483b2a45f73a
jpmoya <juanpablo@rindegastos.com>
2021-12-05 01:55:04 +0100

    Routes, login and css ex ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,40 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  slideOps = {
-    initialSlide: 0,
-    slidesPerView: 1,
-    centeredSlides: true,
-    speed: 400,
-  };
-
-  slides = [
-    {
-    imageSrc: '../../assets/img/logo.png',
-    imageAlt: 'Platzi Music Logo',
-    title: 'Escucha tu música',
-    subTitle: 'EN CUALQUIER LUGAR',
-    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
-    icon: 'play',
-    },
-    {
-    imageSrc: '../../assets/img/logo.png',
-    imageAlt: 'Platzi Music Logo',
-    title: 'Escucha tu música',
-    subTitle: 'EN CUALQUIER LUGAR',
-    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
-    icon: 'play',
-    },
-    {
-    imageSrc: '../../assets/img/logo.png',
-    imageAlt: 'Platzi Music Logo',
-    title: 'Escucha tu música',
-    subTitle: 'EN CUALQUIER LUGAR',
-    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
-    icon: 'play',
-    },
-
-  ];
   constructor() {}
 
 }
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400,
+  };
+
+  slides = [
+    {
+    imageSrc: '../../assets/img/logo.png',
+    imageAlt: 'Platzi Music Logo',
+    title: 'Escucha tu música',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
+    icon: 'play',
+    },
+    {
+    imageSrc: '../../assets/img/logo.png',
+    imageAlt: 'Platzi Music Logo',
+    title: 'Escucha tu música',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
+    icon: 'play',
+    },
+    {
+    imageSrc: '../../assets/img/logo.png',
+    imageAlt: 'Platzi Music Logo',
+    title: 'Escucha tu música',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: 'Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.',
+    icon: 'play',
+    },
+
+  ];
+  constructor(
+    private router: Router,
+    private storage: Storage,
+  ) { }
+  finish(){
+    //console.log('hola');
+    this.storage.create();
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl('/home');
+  }
+  ngOnInit() {
+  }
+
+}
